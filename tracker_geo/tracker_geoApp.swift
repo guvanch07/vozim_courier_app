2712bfd0736b3fbb3f4695cea7162ebc1bb23ff1
Guvanch Amanov <guvanch.amanov@skystart.team>
2023-07-19 01:38:20 +0300

    Initial Commit

---
+++ tracker_geo/tracker_geoApp.swift
@@ -0,0 +1,17 @@
+//
+//  tracker_geoApp.swift
+//  tracker_geo
+//
+//  Created by Guvanch Amanov on 19.07.23.
+//
+
+import SwiftUI
+
+@main
+struct tracker_geoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
